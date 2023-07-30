2e614a2d43893b85a3fb201fccc498c7f10b034f
Paulo Oliveria <paulo.jorge@intelbras.com.br>
2023-07-30 11:20:36 -0300

    uis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val composeVersion = "1.2.1"
+    val coreKtxVersion = "1.9.0"
+    val lifecycleKtxVersion = "2.5.1"
+    val activityComposeVersion = "1.6.0"
+    val androidxLifecycleVersion = "2.5.1"
+    val navigationComposeVersion = "2.5.2"
+
+    implementation("androidx.core:core-ktx:$coreKtxVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleKtxVersion")
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
+
 }
---
+++ app/src/main/java/com/example/stydyandroid2023/designSystem/PrimaryButton.kt
@@ -0,0 +1,53 @@
+package com.example.stydyandroid2023.designSystem
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+//USAGE
+fun main() {
+    /*
+    when (result) {
+        is Result.Success -> Item()
+        is Result.Loading -> Item.Loading()
+        is Result.Error -> Item.Error()
+    }
+    */
+
+}
+
+object PrimaryButton {
+
+    @Composable
+    operator fun invoke(
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+        text: String = "Primary Button",
+    ) {
+        Button(onClick = onClick, modifier = modifier) {
+            Text(text = text)
+        }
+    }
+
+    @Composable
+    fun Large(onClick: () -> Unit) {
+        PrimaryButton(
+            text = "Primary Button Large",
+            onClick = onClick,
+            modifier = Modifier.width(200.dp)
+        )
+    }
+
+    @Composable
+    fun Small(onClick: () -> Unit) {
+        PrimaryButton(
+            text = "Primary Button Small",
+            onClick = onClick,
+            modifier = Modifier.width(100.dp)
+        )
+    }
+}
